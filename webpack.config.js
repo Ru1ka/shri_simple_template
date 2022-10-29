37c86a6df17bfa807c13b5967ca5649361228694
Ru1ka <wotb5ks@gmail.com>
2022-10-29 19:52:40 +0300

    ./public fix and "chunk for lodash"

--- webpack.config.js
+++ webpack.config.js
@@ -8,92 +8,93 @@ require('dotenv').config();
 const ASSET_PATH = process.env.ASSET_PATH || '/';
 
 const config = {
-    resolve: {
-        extensions: ['.js', 'json'],
-        fallback: {
-            buffer: require.resolve('buffer'),
-            crypto: require.resolve('crypto-browserify'),
-            stream: false
-        },
-        alias: {
-            'react-is': path.resolve(__dirname, 'node_modules/react-is')
-        }
+  resolve: {
+    extensions: ['.js', 'json'],
+    fallback: {
+      buffer: require.resolve('buffer'),
+      crypto: require.resolve('crypto-browserify'),
+      stream: false,
     },
-    
-    entry: {
-        index: './src/index.js',
-        about: './src/pages/About.js',
-        home: './src/pages/Home.js',
+    alias: {
+      'react-is': path.resolve(__dirname, 'node_modules/react-is'),
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: "React App",
-            template: './public/index.html'            
-        }),
-        new StatoscopePlugin({
-            name: "shri_simple_template",
-            saveReportTo: 'report.html',
-            saveStatsTo: 'stats.json',
-            saveOnlyStats: false,
-            open: false,
-        }),
-        new webpack.DefinePlugin({
-            'process.env.ASSET_PATH': JSON.stringify(ASSET_PATH),
-          }),
-    ],
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[contenthash].js',
-        publicPath: 'auto',
-    },
-    module: {
-        rules: [
-            // @TODO js rule
-            {
-                test: /\.m?js$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel-loader',
-                options: {
-                    "presets": [
-                        "@babel/preset-env",
-                        ["@babel/preset-react", {"runtime": "automatic"}]]
-                    }
-            },
-            // @TODO css rule
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            // file loader
-            {
-                test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
-                exclude: /node_modules/,
-                use: ['file-loader?name=[name].[ext]'] // ?name=[name].[ext] is only necessary to preserve the original file name
-            }
-        ],
-    },
+  },
 
-    // @TODO optimizations
-    optimization: {
-        // Я не знаю что тут происходит, это всё github copilot, я ему верю
-        usedExports: true,
-        runtimeChunk: {name: 'runtime'},
-        splitChunks: {
-            chunks: 'all',
-            minSize: 0,
-            cacheGroups: {
-                vendors: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: 'vendors',
-                    chunks: 'all',
-                },
-            }
-        }
+  entry: {
+    index: './src/index.js',
+    about: './src/pages/About.js',
+    home: './src/pages/Home.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'React App',
+      template: './public/index.html',
+    }),
+    new StatoscopePlugin({
+      name: 'shri_simple_template',
+      saveReportTo: 'report.html',
+      saveStatsTo: 'stats.json',
+      saveOnlyStats: false,
+      open: false,
+    }),
+    new webpack.DefinePlugin({
+      'process.env.ASSET_PATH': JSON.stringify(ASSET_PATH),
+    }),
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    publicPath: 'auto',
+  },
+  module: {
+    rules: [
+      // @TODO js rule
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            '@babel/preset-env',
+            ['@babel/preset-react', { runtime: 'automatic' }]],
+        },
+      },
+      // @TODO css rule
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      // file loader
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
+        exclude: /node_modules/,
+        use: ['file-loader?name=[name].[ext]'], // ?name=[name].[ext] is only necessary to preserve the original file name
+      },
+    ],
+  },
+
+  // @TODO optimizations
+  optimization: {
+    // Я не знаю что тут происходит, это всё github copilot, я ему верю
+    usedExports: true,
+    runtimeChunk: { name: 'runtime' },
+    splitChunks: {
+      chunks: 'all',
+      minSize: 0,
+      minChunks: 2,
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
     },
-    // @TODO lodash treeshaking Done 👆
-    // @TODO chunk for lodash ?
-    // @TODO chunk for runtime Done 👆
-    // @TODO fallback for crypto Done 👆
+  },
+  // @TODO lodash treeshaking Done 👆
+  // @TODO chunk for lodash Done 👆
+  // @TODO chunk for runtime Done 👆
+  // @TODO fallback for crypto Done 👆
 };
 
 module.exports = config;
